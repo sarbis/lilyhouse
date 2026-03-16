396ee84b53826758a2ad7b6754ee8b4dbda26aa3
ievuce <ieva@sarbantovica.lv>
2026-03-16 23:40:06 +0200

    button fix

--- js/main.js
+++ js/main.js
@@ -368,7 +368,7 @@ function renderItemDetail(item) {
         <div class="item-description">
           <p>${longDescription || description}</p>
         </div>
-        <button class="btn" onclick="openReservationModal('${item.id}', '${name}')">Order</button>
+        <button class="btn" data-translate="item.reserve" onclick="openReservationModal('${item.id}', '${name}')">Order</button>
         <div class="mt-2">
           <a href="collection.html" class="btn btn-secondary">← Back to Collection</a>
         </div>
